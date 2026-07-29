84949d2eb4d78e6c8eacc48ba574097d790b1fca
adam.hsieh <adam.hsieh@kangdainfo.com>
2026-07-29 10:50:13 +0800

    Add @Primary to default RedissonClient bean

--- src/main/java/com/kazama/common/redis/RedissonAutoConfiguration.java
+++ src/main/java/com/kazama/common/redis/RedissonAutoConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -19,6 +20,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 public class RedissonAutoConfiguration {
 
     @Bean
+    @Primary
     @ConditionalOnMissingBean
     public RedissonClient redissonClient(RedisProperties properties) {
         Config config = new Config();
